20d2452a01e3afa3d1d42147d96830a8877ca425
Lyuben Tenekedzhiev <lyuben.tenekedjiev@gmail.com>
2020-03-13 09:42:58 +0100

    after basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { DateTime } from 'luxon';
 
+import Spinner from './components/UI/Spinner/Spinner';
+
+const when = DateTime.local().plus({ days: 1 }).toFormat('ss/mm/hh/dd/MM/yyyy');
+console.log(when)
 function App() {
   return (
     <div className="App">
       <h1>corona</h1>
+      <Spinner />
     </div>
   );
 }
