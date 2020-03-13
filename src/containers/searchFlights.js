1bd7b5db0321973d5a2e6618e0305525d816dff9
soaresilva <ciente@gmail.com>
2020-03-13 10:32:51 +0100

    testing the API

---
+++ src/containers/searchFlights.js
@@ -0,0 +1,24 @@
+import { DateTime } from "luxon";
+
+const searchFlights = async (origin, destination, direct) => {
+  const when = DateTime.local()
+    .plus({ days: 1 })
+    .toFormat("dd/MM/yyyy");
+  const todayDate = DateTime.local().toFormat("dd/MM/yyyy");
+  console.log("todays date", todayDate);
+  console.log("tomorrowsdate", when);
+
+  const query = new URLSearchParams({
+    partner: "picky",
+    v: 3,
+    flyFrom: "WAW",
+    to: "ATH",
+    dateFrom: todayDate,
+    dateTo: when,
+    limit: 5
+  });
+  const url = new URL(`?${query}`, "https://api.skypicker.com/flights");
+  console.log("url", url);
+};
+
+export default searchFlights;
