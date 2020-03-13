86eb6f34a9ef5c88e4f132226f53c5c213a9d4e6
Lyuben Tenekedzhiev <lyuben.tenekedjiev@gmail.com>
2020-03-13 10:47:27 +0100

    after adding a reactstrap dropdown and before displaying flights

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React from "react";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 
-import classes from './LandingPage.module.css';
-import FlightSection from '../FlightSection/FlightSection';
+import classes from "./LandingPage.module.css";
+import FlightSection from "../FlightSection/FlightSection";
 
-const landingPage = ( props ) => {
-  return (
-    <div className={classes.LandingPage}>
-      <div className={classes.LandingPageTitle}>
-        <h1>title goes here and maybe an image</h1>
-        <h2>Searchbars for flights</h2>
+class LandingPage extends React.Component {
+  state = {
+    dropdownOpen: false
+  };
+
+  dropDownClickHandler = () => {
+    this.setState(prevState => {
+      return {
+        dropdownOpen: !prevState.dropdownOpen
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className={classes.LandingPage}>
+        <div className={classes.LandingPageTitle}>
+          <h1>title goes here and maybe an image</h1>
+          <h2>Searchbars for flights</h2>
+
+          {/* <Dropdown isOpen={this.state.dropdownOpen} toggle={this.dropDownClickHandler}>
+            <DropdownToggle caret>Dropdown</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem>Some Action</DropdownItem>
+              <DropdownItem disabled>Action (disabled)</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Foo Action</DropdownItem>
+              <DropdownItem>Bar Action</DropdownItem>
+              <DropdownItem>Quo Action</DropdownItem>
+            </DropdownMenu>
+          </Dropdown> */}
+
+        </div>
+        <FlightSection />
       </div>
-      <FlightSection />
-    </div>
-  )
+    );
+  }
 }
 
-export default landingPage;
+export default LandingPage;
